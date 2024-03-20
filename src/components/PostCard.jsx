3a1d9bc7d2bbfb4f48c26e37f8a61417d35a1986
anandkumar62 <anandkumar101002@gmail.com>
2024-03-20 10:22:58 +0530

    image position

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className='w-full bg-gray-100 rounded-xl p-4'>
-        <div className='w-full justify-center mb-4'>
+      <div className='w-full bg-gray-100 rounded-xl p-4 h-full'>
+        <div className='w-full justify-center mb-4 h-5/6'>
           <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-            className='rounded-xl w-full' />
+            className='rounded-xl w-full h-full bg-center' />
         </div>
         <h2
           className='text-sm font-bold lg:text-lg truncate'
